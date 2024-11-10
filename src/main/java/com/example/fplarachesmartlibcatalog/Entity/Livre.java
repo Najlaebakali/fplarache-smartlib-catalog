97d0cd0966f7b4bee673201522b74ad58a11ebd7
Chmihi <chmihidouae0@gmail.com>
2024-11-10 10:30:05 +0100

    Add Lombok annotations to Livre entity

--- src/main/java/com/example/fplarachesmartlibcatalog/Entity/Livre.java
+++ src/main/java/com/example/fplarachesmartlibcatalog/Entity/Livre.java
@@ -5,10 +5,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
 @Entity
+@Data @AllArgsConstructor @NoArgsConstructor
 public class Livre {
 
 
@@ -25,72 +29,7 @@ public class Livre {
     private LocalDateTime datePublication;
     private String isbn;
 
-    public Livre() {
-    }
 
-    public Livre(Long id, String titre, String auteur, String genre, String description, LocalDateTime datePublication, String isbn) {
-        this.id = id;
-        this.titre = titre;
-        this.auteur = auteur;
-        this.genre = genre;
-        this.description = description;
-        this.datePublication = datePublication;
-        this.isbn = isbn;
-    }
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitre() {
-        return titre;
-    }
-
-    public void setTitre(String titre) {
-        this.titre = titre;
-    }
-
-    public String getAuteur() {
-        return auteur;
-    }
-
-    public void setAuteur(String auteur) {
-        this.auteur = auteur;
-    }
-
-    public String getGenre() {
-        return genre;
-    }
-
-    public void setGenre(String genre) {
-        this.genre = genre;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getDatePublication() {
-        return datePublication;
-    }
-
-    public void setDatePublication(LocalDateTime datePublication) {
-        this.datePublication = datePublication;
-    }
-
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public void setIsbn(String isbn) {
-        this.isbn = isbn;
-    }
 }
